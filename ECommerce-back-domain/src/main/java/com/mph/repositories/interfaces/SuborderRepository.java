b2c57c2b54e7948c2dac7c71041de6e2031d3b0c
m <phamichael123@gmail.com>
2024-02-20 16:12:35 +0100

    feat: add suborder repository

---
+++ ECommerce-back-domain/src/main/java/com/mph/repositories/interfaces/SuborderRepository.java
@@ -0,0 +1,9 @@
+package com.mph.repositories.interfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mph.entities.Suborder;
+
+public interface SuborderRepository extends JpaRepository<Suborder, Long> {
+
+}
